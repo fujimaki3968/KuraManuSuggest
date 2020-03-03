54b179bab0b1dcea142aa7246496a7cd9702cee4
fujimaki3968 <haruki3968@gmail.com>
2020-03-04 01:15:26 +0900

    add random suggest menu

--- Suggestion.py
+++ Suggestion.py
@@ -1,5 +1,5 @@
 import csv
-import random
+from random import randint as R
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
@@ -25,35 +25,36 @@ def binary_search(items, num):
         elif tmp > num:
             right = mid
             mid = left + int((right - left)/2)
-        print(right, left, mid, tmp)
-    print(right)
-    return right
-
-
-ans = binary_search(item_list, 300)
+    return left
 
 
 def main_function(event, context):
-    try:
-        cost = event["money"]
-    except:
-        cost = 1000
-    html = generate_html(item_list, cost)
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/html;charset=UTF-8"},
-        "body": html
-    }
+    if event['money'] != "":
+        money = int(event['money'])
+    else:
+        money = 1000
+    html = generate_html(item_list, money)
+    return html
 
 
 def generate_html(item_list, money):
+    all_money = money
+    suggest_list = []
+    while(all_money>=100):
+        list_range = binary_search(item_list, money)
+        tmp = item_list[R(0, list_range)]
+        suggest_list.append(tmp)
+        all_money -= int(tmp[1])
     tmpl = env.get_template('output.tmpl')
-    html = tmpl.render(items=item_list, money=money, )
+    html = tmpl.render(items=suggest_list, money=money, )
     return html
+#
+# money = 1000
+#
+# html = generate_html(item_list, money)
+#
+# with open('jinja2_test.html',mode='w') as f:
+#     f.write(str(html))
 
-money = 1000
 
-html = generate_html(item_list, money)
 
-with open('jinja2_test.html',mode='w') as f:
-    f.write(str(html))
