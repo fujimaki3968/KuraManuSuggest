9f0b314932d8f0ed964d5270a3699019ebeba5a0
fujimaki3968 <haruki3968@gmail.com>
2020-03-04 00:35:00 +0900

    add items binary search func

--- Suggestion.py
+++ Suggestion.py
@@ -7,7 +7,30 @@ env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
 with open('kura_menu.csv') as fp:
     item_list = list(csv.reader(fp))
 
-print(len(item_list))
+item_list = sorted(item_list, key=lambda x: x[1])
+
+
+
+def binary_search(items, num):
+    length = len(items)
+    mid = int(length / 2)
+    right = length - 1
+    left = 0
+
+    while(right - left > 1):
+        tmp = int(items[mid][1])
+        if tmp <= num:
+            left = mid
+            mid = left + int((right - left)/2)
+        elif tmp > num:
+            right = mid
+            mid = left + int((right - left)/2)
+        print(right, left, mid, tmp)
+    print(right)
+    return right
+
+
+ans = binary_search(item_list, 300)
 
 
 def main_function(event, context):
@@ -28,7 +51,9 @@ def generate_html(item_list, money):
     html = tmpl.render(items=item_list, money=money, )
     return html
 
-# html = generate_html(item_list, money)
-#
-# with open('jinja2_test.html',mode='w') as f:
-#     f.write(str(html))
+money = 1000
+
+html = generate_html(item_list, money)
+
+with open('jinja2_test.html',mode='w') as f:
+    f.write(str(html))
