7649beeb8f801cfe219d17f2375509da8d14a9d2
fujimaki3968 <haruki3968@gmail.com>
2020-03-04 22:21:25 +0900

    add locate value

--- Suggestion.py
+++ Suggestion.py
@@ -29,28 +29,33 @@ def binary_search(items, num):
 def main_function(event, context):
     if event['money'] != "":
         money = int(event['money'])
+        locate = int(event['locate'])
     else:
         money = 1000
-    html = generate_html(item_list, money)
+        locate = 1
+    html = generate_html(item_list, money, locate)
     return html
 
 
-def generate_html(item_list, money):
+def generate_html(item_list, money, locate):
     all_money = money
     sum_cal = 0
     suggest_list = []
     while(all_money >= 100):
         list_range = binary_search(item_list, all_money)
         tmp = item_list[R(0, list_range)]
+        if int(tmp[4]) != 0:
+            if str(locate) not in tmp[4]:
+                continue
         suggest_list.append(tmp)
         sum_cal += int(tmp[3])
         all_money -= int(tmp[1])
 
     tmpl = env.get_template('output.tmpl')
-    html = tmpl.render(items=suggest_list, money=money, sum=money-all_money, cal=sum_cal,)
+    html = tmpl.render(items=suggest_list, money=money, sum=money-all_money, cal=sum_cal, locate=locate,)
     return html
 
-html = generate_html(item_list, 1000)
+html = generate_html(item_list, 5000, 1)
 
 with open('jinja2_test.html',mode='w') as f:
     f.write(str(html))
