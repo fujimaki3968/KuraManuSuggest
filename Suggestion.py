93f4f84d6be3d41f9ec5099acb5c0401a72d3be2
fujimaki3968 <haruki3968@gmail.com>
2020-03-09 22:03:25 +0900

    add tweet button and chenge design

--- Suggestion.py
+++ Suggestion.py
@@ -41,21 +41,33 @@ def generate_html(item_list, money, locate):
     all_money = money
     sum_cal = 0
     suggest_list = []
-    while(all_money >= 100):
+    tweet_text = ""
+    over = False
+    while(all_money >= 110):
         list_range = binary_search(item_list, all_money)
         tmp = item_list[R(0, list_range)]
         if int(tmp[4]) != 0:
             if str(locate) not in tmp[4]:
                 continue
+        if all_money < int(tmp[1])*1.1:
+            continue
+        if len(tweet_text) + len(tmp[0]) < 100:
+            tweet_text += tmp[0] + " "
+        else:
+            over=True
         suggest_list.append(tmp)
         sum_cal += int(tmp[3])
-        all_money -= int(tmp[1])
-
+        all_money -= int(tmp[1]) * 1.1
+    if over:
+        tweet_text += "…"
     tmpl = env.get_template('output.tmpl')
-    html = tmpl.render(items=suggest_list, money=money, sum=money-all_money, cal=sum_cal, locate=locate,)
+    html = tmpl.render(items=suggest_list, money=money, sum=money-int(all_money), cal=sum_cal, locate=locate, tweet_text=tweet_text,)
     return html
 
-html = generate_html(item_list, 5000, 1)
+html = generate_html(item_list, 1000, 1)
+
+for _ in range(1000):
+    html = generate_html(item_list, 1000, 1)
 
 with open('jinja2_test.html',mode='w') as f:
     f.write(str(html))
